43fb8ade5406fae5154dc6a51e451e40c46854dd
Andreas <75949923+anditv21@users.noreply.github.com>
2022-11-27 13:00:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# StartupDelay
+ 
+A small program that starts the Elgato stream deck software at the right time.
+
+If you want to use the ShareX plugin for the StreamDeck together with the Steam version of ShareX it often happens that the Elgato software starts before ShareX.
+<br>
+<br> This triggers an error:
+<br>
+<br>
+<img src="https://i.ibb.co/GT7sMny/image.png">
+
+NOTE:
+<br>
+Make sure that the Elgato software is not in your autostart.
---
+++ startup/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Threading;
+
+namespace startup
+{
+    internal class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        static void Main(string[] args)
+        {
+            //hide console
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+            check();
+        }
+
+        static void check()
+        {
+            Process[] pname = Process.GetProcessesByName("ShareX");
+            if (pname.Length == 0)
+            {
+                Thread.Sleep(1000);
+                check();
+            }
+            else
+            {
+                var process = new Process
+                {
+                    StartInfo = {FileName = "C:\\Program Files\\Elgato\\StreamDeck\\StreamDeck.exe", Arguments = "--runinbk"}
+                };
+                process.Start();
+                RegisterInStartup();
+                Environment.Exit(0);
+            }
+
+        }
+
+        static void RegisterInStartup()
+        {
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            registryKey.SetValue("streamdeckstartup", Application.ExecutablePath);
+        }
+    }
+}
